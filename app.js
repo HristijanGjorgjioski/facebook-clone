3b8b9c6df4da9ca0a535a9a207b0c3b33b415f12
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-02-16 06:56:44 +0100

    created 'add-post' form, now is time to work on her functinality!

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
 const flash = require('connect-flash');
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
 
 const User = require('./models/users');
 
@@ -19,6 +21,23 @@ const store = new MongoDBStore({
 });
 const csrfProtection = csrf();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, res, next) => {
+    cb(null, 'images')
+  },
+  filename: (req, res, next) => {
+    cb(null, uuidv4() + '-' + file.originalName);
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === 'image/png' || file.mimetype === 'image/jpg' || file.mimetype === 'image/jpeg') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -27,7 +46,12 @@ const feedRoutes = require('./routes/feed-routes');
 const postRoutes = require('./routes/post-routes');
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 app.use(session({
   secret: 'secreto',
   resave: false,
