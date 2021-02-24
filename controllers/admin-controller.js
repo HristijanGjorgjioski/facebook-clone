abcbaae3992e70911ab2c7ca465c36ab62546bb8
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-02-24 06:55:08 +0100

    'edit post' functinality is done!

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -15,6 +15,8 @@ exports.getAllPosts = (req, res, next) => {
     })
 }
 
+//////////////////////////////////////////////////
+
 exports.getDeletePost = (req, res, next) => {
   const postId = req.params.postId;
   res.render('admin/all-posts', {
@@ -31,5 +33,46 @@ exports.postDeletePost = (req, res, next) => {
     .then(() => {
       console.log('Product deleted!');
       res.redirect(`/all-posts/${req.user._id}`);
+    })
+}
+
+//////////////////////////////////////////////////
+
+exports.getEditPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+    .then(post => {
+      res.render('admin/edit-post', {
+        pageTitle: 'Edit post',
+        userId: req.user,
+        post: post
+      })
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
+exports.postEditPost = (req, res, next) => {
+  const postId = req.body.postId;
+  const image = req.file;
+  const updatedDescription = req.body.description;
+  Post.findById(postId)
+    .then(post => {
+      post.description = updatedDescription;
+      if(image) {
+        post.imageUrl = image.path
+      }
+      return post.save()
+        .then(result => {
+          console.log('Post is updated');
+          res.redirect(`/all-posts/${req.user._id}`);
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    })
+    .catch(err => {
+      console.log(err);
     })
 }
--- routes/admin-routes.js
+++ routes/admin-routes.js
@@ -6,8 +6,12 @@ const adminController = require('../controllers/admin-controller');
 
 router.get('/all-posts/:userId', adminController.getAllPosts);
 
-router.get('/delete-post/:postId', adminController.postDeletePost);
+router.get('/delete-post/:postId', adminController.getDeletePost);
 
-router.post('/delete-post', adminController.getDeletePost);
+router.post('/delete-post', adminController.postDeletePost);
+
+router.get('/edit-post/:postId', adminController.getEditPost);
+
+router.post('/edit-post', adminController.postEditPost);
 
 module.exports = router;
